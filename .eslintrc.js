0990ae24c26033b0cd045c7216ebb87b931043f7
pozafly <pozafly@gmail.com>
2023-03-24 21:42:11 +0900

    :green_heart: Chore eslint, prettier, husky 설치 및 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'eslint-config-airbnb'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // 화살표 함수 매개변수 1개일 때 괄호 빼줌.
+    'arrow-parens': ['error', 'as-needed'],
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  // 화살표 함수 매개변수 1개일 때 괄호 빼줌.
+  arrowParens: 'avoid',
+};
