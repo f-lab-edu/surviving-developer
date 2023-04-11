9071c20cb941e652291c1dd0f24280f17d3baa2f
pozafly <pozafly@gmail.com>
2023-04-11 17:48:33 +0900

    ♻️ Refactor: controller에서 binding method 관련 리팩토링
    - controller의 bind대상 method 이름을 handle prefix로 변경
    - bind(this) 대상 메서드를 한번에 바인딩 해주는 메서드를 utils로 옮김

---
+++ src/utils/eventUtils.js
@@ -0,0 +1,12 @@
+import { getHasPrefixList } from './stringUtils';
+
+export const bindingMehtods = (instance, prefix) => {
+  const keys = Object.getOwnPropertyNames(Object.getPrototypeOf(instance));
+  const bindMethodNames = getHasPrefixList(prefix, keys);
+  const handlers = {};
+  bindMethodNames.forEach(name => {
+    handlers[name] = instance[name].bind(instance);
+  });
+
+  instance.view.addEvent(handlers);
+};
