9fd8cfb378c9883fd5c0aaa8761085d761b0ea39
pozafly <pozafly@gmail.com>
2023-04-12 18:18:55 +0900

    ✨Feat: LayoutView 추가 및 HeaderView 추가

---
+++ src/view/layouts/Header/headerViewTemplate.js
@@ -0,0 +1,15 @@
+export default () => `
+  <nav>
+    <ul>
+      <li>
+        <a href="/question" data-route="/question">Main</a>
+      </li>
+      <li>
+        <a href="/manage" data-route="/manage">Manage Question</a>
+      </li>
+      <li>
+        <a href="https://github.com/pozafly" target="_blank">Pozafly</a>
+      </li>
+    </ul>
+  </nav>
+`;
