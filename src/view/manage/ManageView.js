aee15df9f595ce0f30086ce0635ef3d06c8b4ad9
pozafly <pozafly@gmail.com>
2023-04-12 18:21:17 +0900

    ✨Feat: Add Manage Page

---
+++ src/view/manage/ManageView.js
@@ -0,0 +1,30 @@
+/**
+ * view
+ *
+ * 1. template
+ * 2. add event
+ * 3. forward events to the controller
+ */
+import manageTemplate from './ManageViewTemplate';
+import View from '../View';
+
+export default class QuestionView extends View {
+  title = null;
+  answer = null;
+
+  constructor() {
+    super(document.querySelector('main'));
+  }
+
+  addEvent(handlers) {
+    this.$newEl.addEventListener('click', this.runDomEvents(handlers), true);
+  }
+
+  runDomEvents() {
+    return () => {};
+  }
+
+  getTemplate() {
+    return manageTemplate();
+  }
+}
---
+++ src/view/manage/ManageViewTemplate.js
@@ -0,0 +1,3 @@
+export default () => `
+  <div>Manage Page</div>
+`;
