8d497a9cf0091a8379975cb4a66ac427caaa702e
pozafly <pozafly@gmail.com>
2023-04-15 22:47:11 +0900

    ✨Feat: router에 broswer title 기능 추가

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,13 +2,16 @@ export default [
   {
     path: '/question/:id',
     name: 'question',
+    title: '질문에 답을 해보시오! 🐳',
   },
   {
     path: '/manage/:filter',
     name: 'manage',
+    title: '질문을 관리 해보시오! 💅',
   },
   {
     path: '/*',
     name: 'notFound',
+    title: '페이지를 찾을 수 없습니다!',
   },
 ];
