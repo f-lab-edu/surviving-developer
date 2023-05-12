c32b305e2317d441c8ac440c3c326315ec5e32a0
pozafly <pozafly@gmail.com>
2023-05-12 10:58:01 +0900

    ✨ Feat: router typescript 변환

---
+++ src/router/routes.ts
@@ -0,0 +1,11 @@
+import { Route } from './types.ts';
+
+const route: Route[] = [
+  {
+    path: '/question/:id',
+    name: 'question',
+    title: '페이지를 찾을 수 없습니다!',
+  },
+];
+
+export default route;
