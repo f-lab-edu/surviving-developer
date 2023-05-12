15a2a20d00c215dfe0cc6f5dabc0a28a3e588116
pozafly <pozafly@gmail.com>
2023-05-12 15:27:20 +0900

    ✨ Feat: indexedDB typescript 변환

--- src/common/IndexedDB.ts
+++ src/common/IndexedDB.ts
@@ -1,11 +1,27 @@
 /* eslint-disable no-console */
-import { randomString } from '../utils/stringUtils';
+import { CATEGORY_TYPE, QUESTION_TYPE } from '../utils/constant.ts';
+import { randomString } from '../utils/stringUtils.ts';
 import questionJSON from './question.json';
 
-const DB_NAME = 'QuestionDB';
-const DB_VERSION = 1;
+type Question = {
+  answer: string;
+  answerList: string[];
+  category: (typeof QUESTION_TYPE)[keyof typeof QUESTION_TYPE];
+  id: string;
+  title: string;
+  type: (typeof CATEGORY_TYPE)[keyof typeof CATEGORY_TYPE];
+};
+
+// eslint-disable-next-line no-shadow
+export enum INDEXED_DB {
+  DB_NAME = 'QuestionDB',
+  DB_VERSION = 3,
+}
 
 export default class IndexedDB {
+  private db!: IDBDatabase;
+  private openRequest!: IDBOpenDBRequest;
+
   constructor() {
     if (!window.indexedDB) {
       console.error('Not Support Browser');
@@ -13,19 +29,21 @@ export default class IndexedDB {
     }
   }
 
-  init() {
+  init(): Promise<IndexedDB> {
     return new Promise((resolve, reject) => {
-      this.openRequest = window.indexedDB.open(DB_NAME, DB_VERSION);
-      this.openRequest.onerror = event => {
-        console.error(`Database error: ${event.target.errorCode}`);
-        reject(event.target.errorCode);
+      this.openRequest = window.indexedDB.open(
+        INDEXED_DB.DB_NAME,
+        INDEXED_DB.DB_VERSION,
+      );
+      this.openRequest.onerror = () => {
+        console.error('Error loading database');
       };
 
-      this.openRequest.onupgradeneeded = event => {
-        this.#upgradeDB(event);
+      this.openRequest.onupgradeneeded = (event: IDBVersionChangeEvent) => {
+        this.upgradeDB(event);
       };
 
-      this.openRequest.onsuccess = async event => {
+      this.openRequest.onsuccess = async (event) => {
         this.db = this.openRequest.result;
         this.db.onversionchange = () => {
           this.db.close();
@@ -38,52 +56,57 @@ export default class IndexedDB {
     });
   }
 
-  #prepareJSON() {
-    const { questionList } = JSON.parse(JSON.stringify(questionJSON));
-    return questionList.map(question => ({
+  private prepareJSON(): Question[] {
+    const { questionList }: { questionList: Question[] } = JSON.parse(
+      JSON.stringify(questionJSON),
+    );
+    return questionList.map((question: Question) => ({
       ...question,
       id: randomString(8),
       answerList: [],
     }));
   }
 
-  #upgradeDB(event) {
+  private upgradeDB(event: IDBVersionChangeEvent): void {
     // DB store 스키마 생성
-    const db = event.target.result;
+    const db = (event.target as any).result;
     if (!db.objectStoreNames.contains('questions')) {
       const questionStore = db.createObjectStore('questions', {
         keyPath: 'id',
       });
 
-      this.#prepareJSON().forEach(question => questionStore.add(question));
+      this.prepareJSON().forEach((question: Question) =>
+        questionStore.add(question),
+      );
     }
   }
 
-  getAll() {
+  getAll(): Promise<Question[]> {
     return new Promise((resolve, reject) => {
       const transaction = this.db.transaction(['questions'], 'readonly');
       const questionStore = transaction.objectStore('questions');
       const request = questionStore.getAll();
 
-      request.onsuccess = event => {
-        const questionList = event.target.result;
+      request.onsuccess = (event) => {
+        const questionList: Question[] = (event.target as any).result;
         resolve(questionList);
       };
 
-      request.onerror = event => {
+      request.onerror = (event) => {
         reject(event.target);
       };
     });
   }
 
-  addAnswer(id, value) {
+  addAnswer(id: Question['id'], value: string): Promise<Question> {
     return new Promise((resolve, reject) => {
       const transaction = this.db.transaction(['questions'], 'readwrite');
       const questionStore = transaction.objectStore('questions');
-      const request = questionStore.get(id);
+      const request: IDBRequest<Question> = questionStore.get(id);
 
       request.onsuccess = () => {
         const data = request.result;
+        console.log(data);
         data.answerList.push(value);
 
         const updateRequest = questionStore.put(data);
@@ -92,41 +115,41 @@ export default class IndexedDB {
         };
       };
 
-      request.onerror = event => {
+      request.onerror = (event: Event) => {
         reject(event.target);
       };
     });
   }
 
-  addQuestion(question) {
+  addQuestion(question: Question): Promise<Question[]> {
     return new Promise((resolve, reject) => {
       const transaction = this.db.transaction(['questions'], 'readwrite');
       const questionStore = transaction.objectStore('questions');
       const request = questionStore.add(question);
 
-      request.onsuccess = event => {
-        const questionList = event.target.result;
+      request.onsuccess = (event) => {
+        const questionList: Question[] = (event.target as any).result;
         resolve(questionList);
       };
 
-      request.onerror = event => {
+      request.onerror = (event) => {
         reject(event.target);
       };
     });
   }
 
-  deleteQuestion(id) {
+  deleteQuestion(id: Question['id']): Promise<Question[]> {
     return new Promise((resolve, reject) => {
       const transaction = this.db.transaction(['questions'], 'readwrite');
       const questionStore = transaction.objectStore('questions');
       const request = questionStore.delete(id);
 
-      request.onsuccess = event => {
-        const questionList = event.target.result;
+      request.onsuccess = (event) => {
+        const questionList = (event.target as any).result;
         resolve(questionList);
       };
 
-      request.onerror = event => {
+      request.onerror = (event) => {
         reject(event.target);
       };
     });
