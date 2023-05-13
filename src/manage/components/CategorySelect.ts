d1a5d4d3b2addd553d8834284c1424f97f69af86
pozafly <pozafly@gmail.com>
2023-05-13 12:47:52 +0900

    ✨ Feat: manage에서 사용되는 compoonent들 TypeScript로 변경

---
+++ src/manage/components/CategorySelect.ts
@@ -0,0 +1,35 @@
+import { CATEGORY_TYPE } from '../../utils/constant.ts';
+
+export type CategorySelectProps = {
+  categoryList: CATEGORY_TYPE;
+  className: string;
+  defaultOption?: string;
+};
+
+export default class QuestionList {
+  props: CategorySelectProps;
+  $element: HTMLSelectElement;
+
+  constructor(props: CategorySelectProps) {
+    this.props = props;
+    this.$element = document.createElement('select');
+    this.createElement();
+  }
+
+  private createElement() {
+    const { categoryList, defaultOption, className } = this
+      .props as CategorySelectProps;
+
+    this.$element.className = className;
+    this.$element.innerHTML = `
+      ${!defaultOption ? `<option value="all" selected>전체</option>` : ''}
+      ${Object.keys(categoryList).map(
+        (category) => `<option value="${category}">${category}</option>`,
+      )}
+    `;
+  }
+
+  get component() {
+    return this.$element;
+  }
+}
